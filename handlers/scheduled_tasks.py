b2defe8794b3d68837b7827c0c53826db2290397
k6mil6 <kamil_6@vk.com>
2023-04-20 22:27:10 +0300

    ===

--- handlers/scheduled_tasks.py
+++ handlers/scheduled_tasks.py
@@ -1,24 +1,19 @@
 import asyncio
-import datetime
+import aioschedule
 
-from aiogram.types import Message
-from aiogram.dispatcher.filters import Command
-from aiogram import Dispatcher
-
-from bot_creation import bot, dp
+from bot_creation import bot
 from config import chat_id
 
-async def send_task_everyday(message: Message):
+async def send_task_everyday():
+    # Сделать БД с заданиями
     everyday_message = 'QQ'
     await bot.send_message(chat_id, everyday_message)
 
-async def scheduled(wait_for):
+async def scheduled():
+    aioschedule.every().day.at("21:44").do(send_task_everyday)
     while True:
-        now = datetime.datetime.now().time()
-        scheduled_time = datetime.time(13, 37, 0)
-        time_to_wait = datetime.datetime.combine(datetime.date.today(), scheduled_time) - datetime.datetime.combine(datetime.date.today(), now)
-        if time_to_wait > wait_for:
-            await asyncio.sleep(time_to_wait.seconds)
-            await send_task_everyday
-def register_handlers_scheduled_tasks(dp: Dispatcher):
-    dp.register_message_handler(scheduled)
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+async def on_startup(_):
+    asyncio.create_task(scheduled())
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import datetime
-
 from aiogram import executor
 from bot_creation import dp
-import asyncio
-
 
 if __name__ == '__main__':
     from handlers import user, admin, scheduled_tasks
     user.register_handlers_user(dp)
     admin.register_handlers_admin(dp)
-    scheduled_tasks.register_handlers_scheduled_tasks(dp)
-    executor.start_polling(dp, skip_updates=True)
+    # scheduled_tasks.register_handlers_scheduled_tasks(dp)
+    executor.start_polling(dp, skip_updates=True, on_startup=scheduled_tasks.on_startup)
